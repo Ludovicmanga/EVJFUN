7feb8ba98ec8b8f059c2cfebe6ba98e46be95e92
Ludovicmanga <ludovic.mangaj@gmail.com>
2025-03-21 11:37:09 +0100

    changed port

--- back/src/main.ts
+++ back/src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 8080;
+
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: ['http://localhost:3000'],
     credentials: true
   });
-  await app.listen(8080);
+  await app.listen(PORT);
 }
 bootstrap();
